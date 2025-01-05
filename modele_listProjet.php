73ca7ce98f8a9b208ec5cfd8d03b8c46b377085f
Ariles Fellah <ariles.fellah937@gmail.com>
2025-01-05 15:00:17 +0100

    Creation de la methode getProjet dans le model

--- modele_listProjet.php
+++ modele_listProjet.php
@@ -4,8 +4,16 @@ include_once 'Connexion.php';
 class ModeleListProjet extends Connexion{
     public function __construct(){}
 
-    public function exemple(){
-        $request = connexion::$database->prepare('Select count(*) from projet');
+    public function getProjets(){
+        $request = connexion::$database->prepare('SELECT DISTINCT projet.idProjet, projet.titre, projet.description, projet.anneeUniversitaire, projet.semestre
+        FROM utilisateur
+        JOIN appartientA ON utilisateur.idUtilisateur = appartientA.idUtilisateur
+        JOIN associeAProjet ON appartientA.idGroupe = associeAProjet.idGroupe
+        JOIN projet ON associeAProjet.idProjet = projet.idProjet
+        WHERE utilisateur.login = :logi AND utilisateur.mdp = :mdp');
+
+        $request->bindParam(':logi', $_GET['login']);
+        $request->bindParam(':mdp', $_GET['pass']);
         $request->execute();
 
         $projet= $request->fetchAll();
