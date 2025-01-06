eb069b0b2b34de471a618576163dd80384f79a70
Ariles Fellah <ariles.fellah937@gmail.com>
2025-01-06 12:49:27 +0100

    Retravaille de la vue : ajout de ligne html

--- moduleEtudiant/vue_listProjet.php
+++ moduleEtudiant/vue_listProjet.php
@@ -4,32 +4,37 @@ class VueListProjet{
 	public function __construct(){}
 
 	public function afficherListProjet($list){
-		echo '<h1>Liste Des Projet</h1>';
-
+?>
+		<h1>Liste Des Projet</h1>
+<?php
 		foreach ($list as $data) {
-			echo '<pre>';
-			echo  "<a href='mod_listProjet.php?action=descrProjet&idProj&id=".$data['idProjet']."'>". $data['idProjet'] .' / '.$data['titre'].  "</a>";  
-			echo '</pres>';
+?>
+			<ul>
+			<li><a href='mod_listProjet.php?action=descrProjet&idProj&id=<?=$data['idProjet']?>'> <?=$data['idProjet']?> / <?=$data['titre']?> </a></li>
+			</ul>
+<?php
 		}    
 	}
 
 	public function afficherDetailProjet($projet){
-		echo '<h1>Détail du Projet</h1>';
-		echo '<pre>';
-		echo '<h4>Nom du Projet</h4>';
-		echo '<ul>'.$projet['titre'].'</ul>';
-		echo '<h4>Description</h4>';
-		echo '<ul>'.$projet['description'].'</ul>';
-		echo '</pre>';
+		?>
+		<h1>Détail du Projet</h1>
+		<h4>Nom du Projet</h4>
+		<li><?=$projet['titre']?></li>
+		<h4>Description</h4>
+		<li><?=$projet['description']?></li>
+		<?php
 	}
 
 	public function affciherProfs($profs){
-		echo '<pre>';
-		echo '<h4>Liste Enseignant</h4>';
+		?>
+		<h4>Liste Enseignant</h4>
+		<?php
 		foreach ($profs as $prof) {
-			echo $prof['login']. "  ";
+		?>
+			<?=$prof['login']?>  
+			<?php
 		}
-		echo '</pre>';
 	}
 } 
 ?>
