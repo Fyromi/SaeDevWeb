9c75497213ee44b7004389ff4d56107677f7f7ce
Ariles Fellah <ariles.fellah937@gmail.com>
2025-01-22 23:19:33 +0100

    Mise a jour du footer

--- composants/footer/vue_footer.php
+++ composants/footer/vue_footer.php
@@ -2,7 +2,14 @@
 class VueCompFooter extends VueCompGenerique {
 
 	public function __construct() {
-		$this->affichage .= '<div id="footer_texte">Blablabla Je suis un Footer</div>';
+		$this->affichage .= '
+    <footer class="text-center mt-4 py-2">
+        <div>
+            <p class="mb-0">© ' . date("Y") . ' Iut de Montreuil - Projet Développement WEB php</p>
+            <small>Site réalisé dans un cadre pédagogique.</small>
+        </div>
+    </footer>';
 	}
+	
 
 }
